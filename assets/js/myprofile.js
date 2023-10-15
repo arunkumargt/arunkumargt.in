360a1743cd1a506d2594b3a66b34572d38dbd3c7
arunkumar.ga <arunkumar.ga@kgisl.com>
2023-10-15 21:50:39 +0530

    Download Resume
    Download resume button click to open and download resume pdf.

--- assets/js/myprofile.js
+++ assets/js/myprofile.js
@@ -7,26 +7,32 @@
 */
 
 // smooth scroll
-$(document).ready(function(){
-    $(".navbar .nav-link").on('click', function(event) {
-
+$(document).ready(function () {
+    $(".navbar .nav-link").on('click', function (event) {
         if (this.hash !== "") {
-
             event.preventDefault();
-
             var hash = this.hash;
-
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 700, function(){
+            }, 700, function () {
                 window.location.hash = hash;
             });
-        } 
+        }
     });
 });
 
 // navbar toggle
-$('#nav-toggle').click(function(){
+$('#nav-toggle').click(function () {
     $(this).toggleClass('is-active')
     $('ul.nav').toggleClass('show');
 });
+
+// Download Resume
+function downloadCV() {
+    var url = './assets/resume/MyResume.pdf';
+    var link = document.createElement('a');
+    link.target = '_blank';
+    link.href = url;
+    link.download = 'file.pdf';
+    link.dispatchEvent(new MouseEvent('click'));
+}
